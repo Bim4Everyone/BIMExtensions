b330619cad4e730810a369372caff7420d1ca522
dosymep <dosymep@gmail.com>
2021-07-02 14:24:13 +0300

    Обновил скрипт на использование общих параметров

--- BIM.tab/РД.panel/RD.stack/Прочее.pulldown/Отверстия и перемычки.pushbutton/script.py
+++ BIM.tab/РД.panel/RD.stack/Прочее.pulldown/Отверстия и перемычки.pushbutton/script.py
@@ -1,77 +1,75 @@
 # -*- coding: utf-8 -*-
-import os.path as op
-import os
-import sys
+import clr
+clr.AddReference('dosymep.Bim4Everyone.dll')
 
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter, Transaction, TransactionGroup,FamilySymbol
+import dosymep
+clr.ImportExtensions(dosymep.Bim4Everyone)
+
 from pyrevit import revit, DB
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter, Transaction, TransactionGroup, FamilySymbol
+from dosymep.Bim4Everyone.SharedParams.SharedParamsConfig import Instance
 
-#__title__ = 'Перенос данных(из Перемычки в Отверстие)'
+familyName = "АР-Отверстие под инженерные системы"
+
+sizeWidthParamName = Instance.SizeWidth
+sizeDepthParamName = Instance.SizeDepth
+
+existsBulkheadParamName = Instance.BulkheadExists
+bulkheadLengthParamName = Instance.BulkheadLength
+bulkheadDepthParamName = Instance.BulkheadDepth
+bulkheadClassParamName = Instance.BulkheadClass
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-#selection = [ doc.GetElement( elId ) for elId in __revit__.ActiveUIDocument.Selection.GetElementIds() ]
+familySymbolIds = [x.Family.GetFamilySymbolIds()\
+    for x in FilteredElementCollector(doc).OfClass(FamilySymbol).ToElements()\
+    if x.Family.Name == familyName] 
 
-famSym = [x.Family.GetFamilySymbolIds() for x in FilteredElementCollector(doc).OfClass(FamilySymbol).ToElements() if x.Family.Name == 'АР-Отверстие под инженерные системы'] 
-#for sym in famSym:
-#	print sym
-emptyList = []
-tg = TransactionGroup(doc, "Update")
-tg.Start()
-t = Transaction(doc, "Update Sheet Parmeters")
-t.Start()
-for fam in famSym:
-	for symid in fam:
-		selection = DB.FilteredElementCollector(revit.doc)\
-					   .WherePasses(DB.FamilyInstanceFilter(revit.doc, symid))\
-					   .ToElements()
-		for el in selection:
-			temp = ['','','']
-			ex_par = el.LookupParameter('Наличие Перемычки').AsInteger()
-			if ex_par:
-				subelId = el.GetSubComponentIds()
-				subel = doc.GetElement(subelId[0])
+transaction = Transaction(doc, "Обновление значений перемычек")
+transaction.Start()
 
-				temp[0] = int(subel.LookupParameter('Speech_Размер_Ширина').AsDouble()*304.8)
+try:
+    emptyList = []
+    for familySymbolId in familySymbolIds:
+        for symbolId in familySymbolId:
+            familyInstances = DB.FilteredElementCollector(revit.doc)\
+                .WherePasses(DB.FamilyInstanceFilter(revit.doc, symbolId))\
+                .ToElements()
+                           
+            for familyInstance in familyInstances:
+                exists = familyInstance.GetParamValue(existsBulkheadParamName)
+                
+                if exists:
+                    subComponentId = familyInstance.GetSubComponentIds()
+                    subComponent = doc.GetElement(subComponentId[0])
+    
+                    width = subComponent.GetParamValue(sizeWidthParamName) * 304.8
+    
+                    subComponentType = doc.GetElement(subComponent.GetTypeId())
+    
+                    depth = subComponentType.GetParamValue(sizeDepthParamName) * 304.8
+                    typeMark = subComponentType.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_MARK).AsString()
+                        
+                    familyInstance.SetParamValue(bulkheadLengthParamName, width)
+                    familyInstance.SetParamValue(bulkheadDepthParamName, depth)
+                    
+                    if typeMark is None:
+                        emptyList.append(familyInstance)
+                    else:
+                        familyInstance.SetParamValue(bulkheadClassParamName, typeMark)
+    
+                else:
+                    familyInstance.SetParamValue(bulkheadLengthParamName, "")
+                    familyInstance.SetParamValue(bulkheadDepthParamName, "")
+                    familyInstance.SetParamValue(bulkheadClassParamName, "")
+    
+    transaction.Commit()
+except Exception as ex:
+    print ex
+    transaction.RollBack()
 
-				subel_type = doc.GetElement(subel.GetTypeId())
-
-				temp[1] = int(subel_type.LookupParameter('Speech_Размер_Глубина').AsDouble()*304.8)
-				temp[2] = subel_type.LookupParameter('Маркировка типоразмера').AsString()
-					
-				pSet = el.Parameters
-				el.LookupParameter('Перемычка Длина').Set(str(temp[0]))
-				el.LookupParameter('Перемычка Глубина').Set(str(temp[1]))
-				if temp[2] is None:
-					emptyList.append(el.Id)
-				else:
-					el.LookupParameter('Перемычка Класс').Set(temp[2])
-
-			else:
-				pSet = el.Parameters
-				el.LookupParameter('Перемычка Длина').Set(str(temp[0]))
-				el.LookupParameter('Перемычка Глубина').Set(str(temp[1]))
-				el.LookupParameter('Перемычка Класс').Set(temp[2])
-			#print temp
-
-t.Commit()
-tg.Assimilate()
-'''
-el = selection[0]
-pSet = el.Parameters
-for param in pSet:
-	name = param.Definition.Name
-	print name
-	hex_name = [elem.encode("hex") for elem in name]
-	print [elem.encode("hex") for elem in '������� ����']
-	print hex_name
-	bin_name = ' '.join(format(ord(x), 'b') for x in name)
-	print ' '.join(format(ord(x), 'b') for x in '������� ����')
-	print bin_name
-	break
-'''
 if emptyList:
-	print "Не заполнен параметр 'Маркировка типоразмера'"
-	for index in emptyList:
-		print index
+    print "Не заполнен параметр 'Маркировка типоразмера'"
+    for index in emptyList:
+        print index.Id
