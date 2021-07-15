dc69f0117cd6db174b0791a5d993b0528e85d92e
dosymep <dosymep@gmail.com>
2021-07-15 14:35:43 +0300

    Добавил импорт пространства имен

--- BIM.tab/Связанные файлы.panel/UnloadAllLinks.pushbutton/script.py
+++ BIM.tab/Связанные файлы.panel/UnloadAllLinks.pushbutton/script.py
@@ -2,6 +2,7 @@
 import clr
 clr.AddReference("dosymep.Revit.dll")
 
+from dosymep.Revit import *
 from pyrevit import forms
 
 selectedFiles = forms.pick_file(files_filter="Revit files (*.rvt)|*.rvt", multi_file=True, title="Выберите Revit файлы")
