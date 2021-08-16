272d6bda22ea17ebea340297cc7f10e521fd5f2d
dosymep <dosymep@gmail.com>
2021-08-16 16:12:19 +0300

    Добавил указание пользователю

--- BIM.tab/Связанные файлы.panel/LinkedElementId.pushbutton/script.py
+++ BIM.tab/Связанные файлы.panel/LinkedElementId.pushbutton/script.py
@@ -14,17 +14,18 @@ from pyrevit import forms
 document = __revit__.ActiveUIDocument.Document
 uiDocument = __revit__.ActiveUIDocument
 
-try:
-    selectedElement = uiDocument.Selection.PickObject(ObjectType.LinkedElement, "Выберите элемент связанного файла.")
-    linkedDocuments = [ documentInstance.GetLinkDocument() for documentInstance in FilteredElementCollector(document).OfClass(RevitLinkInstance) if documentInstance.Id == selectedElement.ElementId ]
-    
-    linkedDocument = next(iter(linkedDocuments), None)
-    if linkedDocument:
-        linkedElement = linkedDocument.GetElement(selectedElement.LinkedElementId)
+with forms.WarningBar(title="Выберите элемент связанного файла"):
+    try:
+        selectedElement = uiDocument.Selection.PickObject(ObjectType.LinkedElement, "Выберите элемент связанного файла.")
+        linkedDocuments = [ documentInstance.GetLinkDocument() for documentInstance in FilteredElementCollector(document).OfClass(RevitLinkInstance) if documentInstance.Id == selectedElement.ElementId ]
 
-        Clipboard.SetText(linkedElement.Id.ToString());
-        print "{title}.rvt ID: {elementId}".format(title=linkedDocument.Title, elementId=linkedElement.Id)
-    else:
-        forms.alert("Не был найден элемент в связанных файлах.", title="Сообщение")
-except OperationCanceledException:
-    pass
+        linkedDocument = next(iter(linkedDocuments), None)
+        if linkedDocument:
+            linkedElement = linkedDocument.GetElement(selectedElement.LinkedElementId)
+
+            Clipboard.SetText(linkedElement.Id.ToString());
+            print "{title}.rvt ID: {elementId}".format(title=linkedDocument.Title, elementId=linkedElement.Id)
+        else:
+            forms.alert("Не был найден элемент в связанных файлах.", title="Сообщение")
+    except OperationCanceledException:
+        pass
