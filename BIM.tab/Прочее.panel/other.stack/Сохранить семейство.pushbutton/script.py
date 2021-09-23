0dc91bc7fd94234ec3c1c9586d838fda72f7d9f4
dosymep <dosymep@gmail.com>
2021-09-23 15:21:20 +0300

    Переписал команду сохранить семейство

--- BIM.tab/Прочее.panel/other.stack/Сохранить семейство.pushbutton/script.py
+++ BIM.tab/Прочее.panel/other.stack/Сохранить семейство.pushbutton/script.py
@@ -1,67 +1,27 @@
 # -*- coding: utf-8 -*-
+
 import os
-from os import walk
 import os.path as op
-import clr
-clr.AddReference("System.Windows.Forms")
-from pyrevit import EXTENSIONS_DEFAULT_DIR
-from System.Windows.Forms import OpenFileDialog, DialogResult
-from System.Windows.Forms import FolderBrowserDialog, DialogResult
+import tempfile
+
+from Autodesk.Revit.DB import *
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 app = doc.Application
-import os
-__title__ = '.'
-FilePath = doc.PathName
-FileName = doc.Title
-#FilePath = FilePath[:FilePath.rindex(FileName)]
 
-TempFilePath = op.join(EXTENSIONS_DEFAULT_DIR, FileName)
+file_path = doc.PathName
+file_name = op.basename(file_path)
+
+temp_file_path = op.join(tempfile.gettempdir(), file_name)
 
 if doc.IsFamilyDocument:
-	doc.SaveAs(TempFilePath)
-	os.remove(FilePath)
-	doc.SaveAs(FilePath)
-	os.remove(TempFilePath)
+	saveOptions = SaveAsOptions()
+	saveOptions.Compact = False
+	saveOptions.OverwriteExistingFile = True
 
-# fileContent = ''
-# filePath = ''
-# openFileDialog = OpenFileDialog()
-
-# openFileDialog.InitialDirectory = "c:\\"
-# openFileDialog.Filter = "rfa files (*.rfa)|*.rfa|All files (*.*)|*.*"
-# openFileDialog.FilterIndex = 2
-# openFileDialog.RestoreDirectory = True
-# if (openFileDialog.ShowDialog() == DialogResult.OK):
-# 	filePath = openFileDialog.FileName
-# 	print filePath
-
-	
-# def reSave(FilePath, FileName):
-	# fileName = op.join(FilePath, FileName)
-	# doc = app.OpenDocumentFile(fileName)
-	# doc.SaveAs(fileName+'_TEMP')
-	# os.remove(fileName)
-	# doc.SaveAs(fileName)
-	# os.remove(fileName+'_TEMP')
-	# try:
-		# doc.Close(False)
-	# except:
-		# pass
-	
-	
-# folderBrowserDialog = FolderBrowserDialog()
-# folderBrowserDialog.Description = "Select the directory that you want to use as the default."
-# folderBrowserDialog.ShowNewFolderButton = False
-# result = folderBrowserDialog.ShowDialog()
-# if result == DialogResult.OK:
-	# folderName = folderBrowserDialog.SelectedPath
-
-	
-# for (dirpath, dirnames, filenames) in walk(folderName):
-	# for filename in filenames:
-		# suffix = filename.split('.')[-1]
-		# if suffix == 'rfa':
-			# reSave(dirpath,filename)
-		
+	doc.SaveAs(temp_file_path, saveOptions)
+	try:
+		doc.SaveAs(file_path, saveOptions)
+	finally:
+		os.remove(temp_file_path)
