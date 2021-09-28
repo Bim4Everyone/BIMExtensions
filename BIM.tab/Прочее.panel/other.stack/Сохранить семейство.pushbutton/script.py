01bb2fe1af2a11481de22ee5a821f3c0a02656e9
dosymep <dosymep@gmail.com>
2021-09-28 10:29:48 +0300

    Добавил удаление бекапов в скрипте сохранения семейств

--- BIM.tab/Прочее.panel/other.stack/Сохранить семейство.pushbutton/script.py
+++ BIM.tab/Прочее.panel/other.stack/Сохранить семейство.pushbutton/script.py
@@ -3,6 +3,7 @@
 import os
 import os.path as op
 import tempfile
+import re
 
 from Autodesk.Revit.DB import *
 
@@ -15,6 +16,20 @@ file_name = op.basename(file_path)
 
 temp_file_path = op.join(tempfile.gettempdir(), file_name)
 
+
+def remove_backups():
+	# удаляем свой бекап файла ревита
+	os.remove(temp_file_path)
+
+	# удаляем бекапы созданные ревитом
+	dir_name = op.dirname(file_path)
+	backup_files = [op.join(dir_name, f) for f in os.listdir(dir_name)
+					if f.startswith(doc.Title) and f != file_name and re.findall(r".*\.\d\d\d\d\..*", f) ]
+	backup_files = [f for f in backup_files if op.isfile(f) ]
+	for backup_file in backup_files:
+		os.remove(backup_file)
+
+
 if doc.IsFamilyDocument:
 	saveOptions = SaveAsOptions()
 	saveOptions.Compact = False
@@ -25,4 +40,4 @@ if doc.IsFamilyDocument:
 	try:
 		doc.SaveAs(file_path, saveOptions)
 	finally:
-		os.remove(temp_file_path)
+		remove_backups()
