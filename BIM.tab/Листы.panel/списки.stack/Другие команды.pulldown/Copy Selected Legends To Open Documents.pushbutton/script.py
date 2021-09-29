c01ceb3a5bd39fc12f29ffed0473ec441ae22bd3
dosymep <dosymep@gmail.com>
2021-09-29 13:38:48 +0300

    Добавил скрипт копирования легенд

--- BIM.tab/Листы.panel/списки.stack/Другие команды.pulldown/Copy Selected Legends To Open Documents.pushbutton/script.py
+++ BIM.tab/Листы.panel/списки.stack/Другие команды.pulldown/Copy Selected Legends To Open Documents.pushbutton/script.py
@@ -1,106 +1,99 @@
-# -*- coding: utf-8 -*-
-"""Copies selected legend views to all projects currently open in Revit."""
-
+# coding=utf-8
+#pylint: disable=E0401,W0613,C0103,C0111
+import sys
 from pyrevit.framework import List
-from pyrevit import revit, DB, UI
+from pyrevit import revit, DB
 from pyrevit import script
 from pyrevit import forms
 
 
-#__helpurl__ = 'https://www.youtube.com/watch?v=ThzcRM_Tj8g'
-__doc__ = 'Копирует выбранные легенды в другой открытый проект'
-__title__ = 'Легенды в проект'
+logger = script.get_logger()
+
 
 class CopyUseDestination(DB.IDuplicateTypeNamesHandler):
     def OnDuplicateTypeNamesFound(self, args):
         return DB.DuplicateTypeAction.UseDestinationTypes
 
 
-def error(msg):
-    forms.alert(msg)
-    script.exit()
-
-import sys
-import clr
-
-clr.AddReference('System')
-clr.AddReference('System.IO')
-clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import MessageBox
-
-def alert(msg):
-    MessageBox.Show(msg)
-
 # find open documents other than the active doc
-open_docs = [d for d in revit.docs if not d.IsLinked]
-open_docs.remove(revit.doc)
-if len(open_docs) < 1:
-    error('Открыт всего один проект. '
-          'Должно быть открыто два проекта.')
+open_docs = forms.select_open_docs(title='Выберите целевые документы')
+if not open_docs:
+    sys.exit(0)
 
 # get a list of selected legends
-selection = [x for x in revit.get_selection()
-             if x.ViewType == DB.ViewType.Legend]
+legends = forms.select_views(
+    title='Выберите чертежные виды',
+    filterfunc=lambda x: x.ViewType == DB.ViewType.Legend,
+    use_selection=True)
 
-if len(selection) > 0:
+if legends:
     for dest_doc in open_docs:
-        print('\n---PROCESSING DOCUMENT {0}---'.format(dest_doc.Title))
-        # finding first available legend view
-        base_legend_view = None
-        for v in DB.FilteredElementCollector(dest_doc).OfClass(DB.View):
-            if v.ViewType == DB.ViewType.Legend:
-                base_legend_view = v
-                break
+        # get all views and collect names
+        all_graphviews = revit.query.get_all_views(doc=dest_doc)
+        all_legend_names = [revit.query.get_name(x)
+                            for x in all_graphviews
+                            if x.ViewType == DB.ViewType.Legend]
 
-        if base_legend_view is None:
-            error('Document\n{0}\nmust have at least one Legend view.'
-                  .format(dest_doc.Title))
+        print('Обработка документа: {0}'.format(dest_doc.Title))
+        # finding first available legend view
+        base_legend = revit.query.find_first_legend(doc=dest_doc)
+        if not base_legend:
+            forms.alert('В целевом документе должна быть хотя бы одна легенда.',
+                        exitscript=True)
+
         # iterate over interfacetypes legend views
-        for source_view in selection:
-            print('\nКопируется {0}'.format(source_view.ViewName))
+        for src_legend in legends:
+            print('\tКопирование: {0}'.format(revit.query.get_name(src_legend)))
             # get legend view elements and exclude non-copyable elements
-            viewElements = \
-                DB.FilteredElementCollector(revit.doc, source_view.Id)\
+            view_elements = \
+                DB.FilteredElementCollector(revit.doc, src_legend.Id)\
                   .ToElements()
 
-            element_list = []
-            for el in viewElements:
+            elements_to_copy = []
+            for el in view_elements:
                 if isinstance(el, DB.Element) and el.Category:
-                    element_list.append(el.Id)
+                    elements_to_copy.append(el.Id)
                 else:
-                    print('Присвоенный ID: {0}'.format(el.Id))
-            if len(element_list) < 1:
-                print('Проверка содержимого {0}. Элементы в легенде не найдены.'
-                      .format(source_view.ViewName))
+                    logger.debug('Skipping element: %s', el.Id)
+            if not elements_to_copy:
+                logger.debug('Skipping empty view: %s',
+                             revit.query.get_name(src_legend))
                 continue
 
             # start creating views and copying elements
-            with revit.Transaction('Copy Legends to this document',
+            with revit.Transaction('Копирование легенд',
                                    doc=dest_doc):
-                destView = dest_doc.GetElement(
-                    base_legend_view.Duplicate(
+                dest_view = dest_doc.GetElement(
+                    base_legend.Duplicate(
                         DB.ViewDuplicateOption.Duplicate
                         )
                     )
 
                 options = DB.CopyPasteOptions()
                 options.SetDuplicateTypeNamesHandler(CopyUseDestination())
-                copied_element = \
+                copied_elements = \
                     DB.ElementTransformUtils.CopyElements(
-                        source_view,
-                        List[DB.ElementId](element_list),
-                        destView,
+                        src_legend,
+                        List[DB.ElementId](elements_to_copy),
+                        dest_view,
                         None,
                         options)
 
                 # matching element graphics overrides and view properties
-                for dest, src in zip(copied_element, element_list):
-                    destView.SetElementOverrides(
+                for dest, src in zip(copied_elements, elements_to_copy):
+                    dest_view.SetElementOverrides(
                         dest,
-                        source_view.GetElementOverrides(src)
+                        src_legend.GetElementOverrides(src)
                         )
 
-                destView.ViewName = source_view.ViewName
-                destView.Scale = source_view.Scale
-else:
-    error('Хотя бы одна легенда должна быть выбрана.')
+                # matching view name and scale
+                src_name = revit.query.get_name(src_legend)
+                count = 0
+                new_name = src_name
+                while new_name in all_legend_names:
+                    count += 1
+                    new_name = src_name + ' (Копия %s)' % count
+                    logger.warning(
+                        'Легенда уже существует. Переименование в: "%s"', new_name)
+                revit.update.set_name(dest_view, new_name)
+                dest_view.Scale = src_legend.Scale
