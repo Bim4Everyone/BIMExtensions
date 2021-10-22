d25c15672c06fd94c2b637a61e318a448915fb78
dosymep <dosymep@gmail.com>
2021-10-22 09:26:22 +0300

    Обновил загрузку библиотек фреймворка. Разделил библиотеки по версии Revit

--- BIM.tab/Установки.panel/settings.stack/Settings.smartbutton/script.py
+++ BIM.tab/Установки.panel/settings.stack/Settings.smartbutton/script.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import dosymep_libs
+dosymep_libs.load_assemblies()
+
 import clr
+clr.AddReference('dosymep.Revit.dll')
 clr.AddReference('dosymep.Bim4Everyone.dll')
 
 from pyrevit import HOST_APP
 from pyrevit import EXEC_PARAMS
-from pyrevit import script
-from pyrevit import forms
-from pyrevit.loader import sessionmgr
-from pyrevit.loader import sessioninfo
 from pyrevit.userconfig import user_config
 from Autodesk.Revit.ApplicationServices import LanguageType
 
 from dosymep.Bim4Everyone.SharedParams import SharedParamsConfig
 from dosymep.Bim4Everyone.ProjectParams import ProjectParamsConfig
 
-
 if HOST_APP.version == "2020":
-    clr.AddReference('PlatformSettings.dll')
-elif HOST_APP.version == "2021":
-    clr.AddReference('PlatformSettings_2021.dll')
-elif HOST_APP.version == "2022":
-    clr.AddReference('PlatformSettings_2022.dll')
+    clr.AddReference("PlatformSettings.dll")
+else:
+    clr.AddReference("PlatformSettings_{}.dll".format(HOST_APP.version))
 
 import PlatformSettings
+
 
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
     user_config.auto_update = True
@@ -36,43 +34,33 @@ def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
     if __rvt__.Application.Language == LanguageType.English_USA:
         user_config.user_locale = 'en_us'
 
-    LoadPlatformSettings()
+    load_platform_settings()
+
+def get_config_path(section, option):
+    try:
+        if user_config.has_section(section):
+            return user_config.get_section(section).get_option(option)
+    except:
+        pass
 
 
-def LoadPlatformSettings():
-    sharedParamsPath = None
-    projectParamsPath = None
-    if user_config.has_section("PlatformSettings"):
-        try:
-            sharedParamsPath = user_config.get_section("PlatformSettings").get_option("SharedParamsPath")
-        except:
-            pass
-            
-        try:
-            projectParamsPath = user_config.get_section("PlatformSettings").get_option("ProjectParamsPath")
-        except:
-            pass
-    
-    SharedParamsConfig.LoadInstance(sharedParamsPath)
-    ProjectParamsConfig.LoadInstance(projectParamsPath)
+def load_platform_settings():
+    shared_params_path = get_config_path("PlatformSettings", "SharedParamsPath")
+    project_params_path = get_config_path("PlatformSettings", "ProjectParamsPath")
 
+    SharedParamsConfig.LoadInstance(shared_params_path)
+    ProjectParamsConfig.LoadInstance(project_params_path)
 
-def OpenPlatrormSettings():
+def open_platform_settings():
     settings = PlatformSettings.PlatformSettingsCommand()
     result = settings.Execute(EXEC_PARAMS.command_data)
 
     if result:
-        user_config.reload()    
-        LoadPlatformSettings()
-        
-        logger = script.get_logger()
-        results = script.get_results()
+        user_config.reload()
+        load_platform_settings()
 
-        # re-load pyrevit session.
-        logger.info('Reloading....')
-        sessionmgr.load_session()
-
-        results.newsession = sessioninfo.get_session_uuid()
+        from pyrevit.loader.sessionmgr import execute_command
+        execute_command("01dotbim-bim-установки-settings-reload")
 
 if __name__ == '__main__':
-    OpenPlatrormSettings()
+    open_platform_settings()
