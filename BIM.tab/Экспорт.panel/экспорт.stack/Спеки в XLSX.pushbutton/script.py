bae30ecf028e54a7cd283ed2ff2aa6273272194e
dosymep <biseuv@yandex.ru>
2022-01-27 11:24:15 +0300

    Обновил команду выгрузки спецификаций

--- BIM.tab/Экспорт.panel/экспорт.stack/Спеки в XLSX.pushbutton/script.py
+++ BIM.tab/Экспорт.panel/экспорт.stack/Спеки в XLSX.pushbutton/script.py
@@ -1,172 +1,325 @@
 # -*- coding: utf-8 -*-
+import os
 
 import clr
 clr.AddReference('System')
-clr.AddReference('System.Drawing')
 clr.AddReference('System.IO')
-clr.AddReference("System.Windows.Forms")
-clr.AddReference("EPPlus")
+clr.AddReference("PresentationCore")
+clr.AddReference("PresentationFramework")
+
+clr.AddReference("EPPlus.dll")
+clr.AddReference("Microsoft.IO.RecyclableMemoryStream.dll")
+
+clr.AddReference("dosymep.Revit.dll")
+clr.AddReference("dosymep.Bim4Everyone.dll")
+
+import dosymep
+clr.ImportExtensions(dosymep.Revit)
+clr.ImportExtensions(dosymep.Bim4Everyone)
 
 from System.IO import FileInfo
+from System.Windows.Media import VisualTreeHelper
+from System.Windows.Data import CollectionViewSource, PropertyGroupDescription
+
 from OfficeOpenXml import *
 from Autodesk.Revit.DB import *
+
+
+from dosymep.Bim4Everyone.Templates import ProjectParameters
+from dosymep.Bim4Everyone.ProjectParams import ProjectParamsConfig
+
 
 from pyrevit import forms
 from pyrevit import script
 
+
 def FilterString(obj):
-	res = obj
-	unacceptableSymbols = ['/', '\\', ':', '*', '<', '>', '|']
-	for symbol in unacceptableSymbols:
-		temp = res.split(symbol)
-		res = "".join(temp)
-	
-	return res
+    res = obj
+    unacceptable_symbols = ['/', '\\', ':', '*', '<', '>', '|']
+    for symbol in unacceptable_symbols:
+        temp = res.split(symbol)
+        res = "".join(temp)
+
+    return res
+
+
+class Excel(object):
+    def __init__(self, obj, name):
+        self.excel = obj
+        self.name = name
+
 
 class TabelsConverter(object):
-	def __init__(self, view):
-		self.__view = view
-		self.__data = view.GetTableData()
-		self.__currentRow = 1
-		self.__currentColumn = 1
-		self.package = self.__createPackage()
-		self.__currentWorksheet = self.package.Workbook.Worksheets.Add(FilterString(view.Name))
-		self.__exportToExcel()
+    def __init__(self, views, save2_1file):
+        excel_files = []
+        excel_file = Excel(self.__create_package(), views[0].Name)
+        for v in views:
+            self.__currentRow = 1
+            self.__currentColumn = 1
+            data = v.GetTableData()
+            if not save2_1file:
+                excel_file = Excel(self.__create_package(), v.Name)
+                excel_files.append(excel_file)
 
-	def __createPackage(self):
-		return ExcelPackage()
-	
-	def __exportToExcel(self):
-		self.__alignCells()
-		self.__exportSection(SectionType.Header)
-		self.__exportSection(SectionType.Body)
-	
-	def __alignCells(self):
-		sectionData = self.__data.GetSectionData(SectionType.Body)
-		numberOfColumns = sectionData.NumberOfColumns
-		for columnNumber in range(numberOfColumns):
-			columnWidth = sectionData.GetColumnWidthInPixels(columnNumber) / 6
-			self.__currentWorksheet.Column(columnNumber + 1).Width = columnWidth
+            current_worksheet = excel_file.excel.Workbook.Worksheets.Add(FilterString(v.Name))
+            self.__export_to_excel(data, current_worksheet, v)
 
-	def __exportSection(self, sectionType):
-		sectionData = self.__data.GetSectionData(sectionType)
+        if save2_1file:
+            self.excel_file = [excel_file]
+        else:
+            self.excel_file = excel_files
 
-		numberOfRows = sectionData.NumberOfRows
-		numberOfColumns = sectionData.NumberOfColumns
-		firstRow = sectionData.FirstRowNumber 
-		firstColumn = sectionData.FirstColumnNumber
+    def __create_package(self):
+        return ExcelPackage()
 
-		for rowNumber in range(firstRow, firstRow + numberOfRows):
-			rowHeight = sectionData.GetRowHeightInPixels(rowNumber)
-			self.__currentWorksheet.Row(self.__currentRow).Height = rowHeight
-			for columnNumber in range(firstColumn, firstColumn + numberOfColumns):
-				self.__exportCell(rowNumber, columnNumber, sectionType)
-				self.__currentColumn += 1
-			self.__currentRow += 1
-			self.__currentColumn = 1
+    def __export_to_excel(self, data, current_worksheet, v):
+        self.__align_cells(data, current_worksheet)
+        self.__export_section(SectionType.Header, data, current_worksheet, v)
+        self.__export_section(SectionType.Body, data, current_worksheet, v)
 
-	def __exportCell(self, row, column, sectionType):
-		sectionData = self.__data.GetSectionData(sectionType)
-		mergetCell = sectionData.GetMergedCell(row, column)
-		if row == mergetCell.Top and column == mergetCell.Left:
-			text = self.__view.GetCellText(sectionType, row, column)
-			
+    def __align_cells(self, data, current_worksheet):
+        section_data = data.GetSectionData(SectionType.Body)
+        number_of_columns = section_data.NumberOfColumns
+        for columnNumber in range(number_of_columns):
+            column_width = section_data.GetColumnWidthInPixels(columnNumber) / 6
+            current_worksheet.Column(columnNumber + 1).Width = column_width
 
-			cellStyle = sectionData.GetTableCellStyle(row, column)
+    def __export_section(self, section_type, data, current_worksheet, v):
+        section_data = data.GetSectionData(section_type)
 
-			excelCell = self.__currentWorksheet.Cells[self.__currentRow, self.__currentColumn]
+        number_of_rows = section_data.NumberOfRows
+        number_of_columns = section_data.NumberOfColumns
+        first_row = section_data.FirstRowNumber
+        first_column = section_data.FirstColumnNumber
 
-			excelCell.Value = text
+        for row_number in range(first_row, first_row + number_of_rows):
+            row_height = section_data.GetRowHeightInPixels(row_number)
+            current_worksheet.Row(self.__currentRow).Height = row_height
+            for columnNumber in range(first_column, first_column + number_of_columns):
+                self.__export_cell(row_number, columnNumber, section_type, data, current_worksheet, v)
+                self.__currentColumn += 1
+            self.__currentRow += 1
+            self.__currentColumn = 1
 
-			splitText = text.split(",")
-			if len(splitText) < 3:
-				# print "may digit"
-				if all(map(lambda x: x.isdigit(), splitText)):
-					excelCell.Value = float(".".join(splitText))
-					if len(splitText) == 1:
-						excelCell.Style.Numberformat.Format = "0"
-					else:
-						excelCell.Style.Numberformat.Format = "0." + "0" * len(splitText[1])
-					# print "DIGIT!!"
+    def __export_cell(self, row, column, section_type, data, current_worksheet, v):
+        section_data = data.GetSectionData(section_type)
+        merget_cell = section_data.GetMergedCell(row, column)
+        if row == merget_cell.Top and column == merget_cell.Left:
+            text = v.GetCellText(section_type, row, column)
 
-			excelCell.Style.Font.Bold = cellStyle.IsFontBold
-			excelCell.Style.Font.Italic = cellStyle.IsFontItalic
-			excelCell.Style.Font.UnderLine = cellStyle.IsFontUnderline
-			excelCell.Style.Font.Size = cellStyle.TextSize
-			excelCell.Style.Font.Name = cellStyle.FontName
-			excelCell.Style.WrapText = True
+            cell_style = section_data.GetTableCellStyle(row, column)
 
-			horizontalAlignment = cellStyle.FontHorizontalAlignment
-			if horizontalAlignment == HorizontalAlignmentStyle.Center:
-				excelCell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Center
-			elif horizontalAlignment == HorizontalAlignmentStyle.Left:
-				excelCell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Left
-			else:
-				excelCell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Right
+            excel_cell = current_worksheet.Cells[self.__currentRow, self.__currentColumn]
 
-			verticalAlignment = cellStyle.FontVerticalAlignment 
-			if verticalAlignment == VerticalAlignmentStyle.Top:
-				excelCell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Top
-			elif verticalAlignment == VerticalAlignmentStyle.Middle:
-				excelCell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Center
-			else:
-				excelCell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Bottom
-			
-			# param = self.__view.Document.GetElement(sectionData.GetCellParamId(row,
-			# column))
-			# if param:
-			# 	if isinstance(param, Parameter):
-			# 		print "-- {} - {}".format(param.Definition.Name, param.StorageType)
-			# 	else:
-			# 		definition = param.GetDefinition()
-			# 		print "*****{}*".format(definition.Name)
-			rowRange = mergetCell.Bottom - row
-			columnRange = mergetCell.Right - column
-			if rowRange > 0 or columnRange > 0:
-				# print "Merge {},{} - {},{}".format(rowNumber, columnNumber,
-				# mergetCell.Bottom, mergetCell.Right)
-				self.__currentWorksheet.Cells[self.__currentRow, self.__currentColumn, self.__currentRow + rowRange, self.__currentColumn + columnRange].Merge = True
+            excel_cell.Value = text
+
+            split_text = text.split(",")
+            if len(split_text) < 3:
+                # print "may digit"
+                if all(map(lambda x: x.isdigit(), split_text)):
+                    excel_cell.Value = float(".".join(split_text))
+                    if len(split_text) == 1:
+                        excel_cell.Style.Numberformat.Format = "0"
+                    else:
+                        excel_cell.Style.Numberformat.Format = "0." + "0" * len(split_text[1])
+                # print "DIGIT!!"
+
+            excel_cell.Style.Font.Bold = cell_style.IsFontBold
+            excel_cell.Style.Font.Italic = cell_style.IsFontItalic
+            excel_cell.Style.Font.UnderLine = cell_style.IsFontUnderline
+            excel_cell.Style.Font.Size = cell_style.TextSize
+            excel_cell.Style.Font.Name = cell_style.FontName
+            excel_cell.Style.WrapText = True
+
+            horizontal_alignment = cell_style.FontHorizontalAlignment
+            if horizontal_alignment == HorizontalAlignmentStyle.Center:
+                excel_cell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Center
+            elif horizontal_alignment == HorizontalAlignmentStyle.Left:
+                excel_cell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Left
+            else:
+                excel_cell.Style.HorizontalAlignment = Style.ExcelHorizontalAlignment.Right
+
+            vertical_alignment = cell_style.FontVerticalAlignment
+            if vertical_alignment == VerticalAlignmentStyle.Top:
+                excel_cell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Top
+            elif vertical_alignment == VerticalAlignmentStyle.Middle:
+                excel_cell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Center
+            else:
+                excel_cell.Style.VerticalAlignment = Style.ExcelVerticalAlignment.Bottom
+
+            row_range = merget_cell.Bottom - row
+            column_range = merget_cell.Right - column
+            if row_range > 0 or column_range > 0:
+                current_worksheet.Cells[self.__currentRow,
+                                        self.__currentColumn, self.__currentRow + row_range, self.__currentColumn + column_range].Merge = True
 
 
 class CheckBoxOption:
-	def __init__(self, obj):
-		self.state = False
-		self.name = obj.Name
-		self.obj = obj
+    def __init__(self, obj):
+        self.state = False
+        self.name = obj.Name
+        self.obj = obj
+        self.view_assignment = obj.GetParamValueOrDefault(ProjectParamsConfig.Instance.ViewGroup)
+        self.view_assignment = self.view_assignment if self.view_assignment else None
 
-	def __nonzero__(self):
-		return self.state
+    def __nonzero__(self):
+        return self.state
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
+
+class PrintSheetsWindow(forms.WPFWindow):
+    def __init__(self, xaml_file_name, **kwargs):
+        forms.WPFWindow.__init__(self, xaml_file_name)
+
+        self.sheet_list = kwargs.get('list', None)
+
+    # doc and schedule
+    @property
+    def selected_doc(self):
+        selected_doc = self.documents_cb.SelectedItem
+        for open_doc in revit.docs:
+            if open_doc.GetHashCode() == selected_doc.hash:
+                return open_doc
+
+    # sheet list
+    @property
+    def sheet_list(self):
+        return self.sheets_lb.ItemsSource
+
+    @sheet_list.setter
+    def sheet_list(self, value):
+        self.sheets_lb.ItemsSource = value
+
+    @property
+    def selected_sheets(self):
+        return self.sheets_lb.SelectedItems
+
+    def _verify_context(self):
+        new_context = []
+        for item in self.sheet_list:
+            if not hasattr(item, 'state'):
+                new_context.append(BaseCheckBoxItem(item))
+            else:
+                new_context.append(item)
+
+        self.sheet_list = new_context
+
+    def _set_states(self, state=True, flip=False, selected=False):
+        all_items = self.sheet_list
+
+        if selected:
+            current_list = self.selected_sheets
+        else:
+            current_list = self.sheet_list
+        for checkbox in current_list:
+            if flip:
+                checkbox.state = not checkbox.state
+            else:
+                checkbox.state = state
+
+        # push list view to redraw
+        self.sheet_list = []
+        self.sheet_list = all_items
+
+    def toggle_all(self, sender, args):
+        """Handle toggle all button to toggle state of all check boxes."""
+        self._set_states(flip=True)
+
+    def check_all(self, sender, args):
+        """Handle check all button to mark all check boxes as checked."""
+        self._set_states(state=True)
+
+    def uncheck_all(self, sender, args):
+        """Handle uncheck all button to mark all check boxes as un-checked."""
+        self._set_states(state=False)
+
+    def check_selected(self, sender, args):
+        """Mark selected checkboxes as checked."""
+        border = VisualTreeHelper.GetChild(self.sheets_lb, 0)
+        self.scroll_viewer = border.Child
+        offset = self.scroll_viewer.VerticalOffset
+        self._set_states(state=True, selected=True)
+        self.scroll_viewer.ScrollToVerticalOffset(offset)
+
+    def uncheck_selected(self, sender, args):
+        """Mark selected checkboxes as unchecked."""
+        border = VisualTreeHelper.GetChild(self.sheets_lb, 0)
+        self.scroll_viewer = border.Child
+        offset = self.scroll_viewer.VerticalOffset
+        self._set_states(state=False, selected=True)
+        self.scroll_viewer.ScrollToVerticalOffset(offset)
+
+    def button_select(self, sender, args):
+        """Handle select button click."""
+        try:
+            sheets = [x for x in self.sheet_list if x.state]
+            if len(sheets) > 0:
+                self.response = {
+                    'sheets': sheets,
+                    'oneFile': self.oneFile.IsChecked}
+                self.Close()
+            else:
+                forms.alert('Вы должны выбрать хотя бы один лист.')
+        except:
+            script.exit()
+
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 app = __revit__.Application
-view = __revit__.ActiveUIDocument.ActiveGraphicalView 
+view = __revit__.ActiveUIDocument.ActiveGraphicalView
 view = doc.ActiveView
 
-viewSchedules = FilteredElementCollector(doc)\
-	.OfClass(ViewSchedule)\
-	.ToElements()
-
+viewSchedules = FilteredElementCollector(doc).OfClass(ViewSchedule).ToElements()
 items = [CheckBoxOption(x) for x in viewSchedules]
-items = sorted(items, key=lambda item: item.name)
+items = sorted(items, key=lambda item: (item.view_assignment, item.name))
 
-res = forms.SelectFromList.show(items, button_name='Выбрать', multiselect=True)
-if not res:
-	script.exit()
+# items = sorted(items, key=lambda item: item.name)
+
+view = CollectionViewSource.GetDefaultView(items)
+groupDescription = PropertyGroupDescription('view_assignment')
+view.GroupDescriptions.Add(groupDescription)
+
+window = PrintSheetsWindow('selectViews.xaml', list=view)
+window.ShowDialog()
+
+sel_sheets = ''
+if hasattr(window, 'response'):
+    sheets = window.response
+    sel_sheets = sheets['sheets']
+    save2_1file = sheets['oneFile']
+
+if not sel_sheets:
+    script.exit()
 
 folder_name = forms.pick_folder(title="Выберите папку для сохранения спецификаций")
 if folder_name:
-	for item in res:
-		fileInfo = FileInfo(folder_name + "\\" + FilterString(item.name) + ".xlsx")
+    views = []
+    file_info = ''
 
-		converter = TabelsConverter(item.obj)
-		with converter.package as package:
-			try:
-				package.SaveAs(fileInfo)
-				package.Save()
-			except Exception as ex:
-				print "Не удалось сохранить спецификацию '{}'".format(item.name)
-				print "Исключение '{}'".format(ex)
+    for item in sel_sheets:
+        if file_info == '':
+            file_info = FileInfo(folder_name + "\\" + FilterString(item.name) + ".xlsx")
+        views.append(item.obj)
+
+    converter = TabelsConverter(views, save2_1file)
+    package = converter.excel_file
+    try:
+        count = 1
+        for p in package:
+            path_file = folder_name + "\\" + FilterString(p.name) + ".xlsx"
+            count = 1
+            while os.path.isfile(path_file):
+                path_file = folder_name + "\\" + FilterString(p.name) + "-" + str(count) + ".xlsx"
+                count += 1
+
+            file_info = FileInfo(path_file)
+            p.excel.SaveAs(file_info)
+            p.excel.Save()
+            p.excel.Dispose()
+
+    except Exception as ex:
+        print "Не удалось сохранить спецификацию '{}'".format(item.name)
+        print "Исключение '{}'".format(ex)
