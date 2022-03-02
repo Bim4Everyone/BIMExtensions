3a51ffcf873937d47a16df16e2fee6ad0f85044e
dosymep <dosymep@gmail.com>
2022-03-02 14:22:42 +0300

    Исправил проблему с автоматическим выбором языка

--- BIM.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
+++ BIM.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
@@ -4,6 +4,8 @@ import clr
 clr.AddReference('dosymep.Revit.dll')
 clr.AddReference('dosymep.Bim4Everyone.dll')
 
+from Autodesk.Revit.ApplicationServices import LanguageType
+
 from pyrevit import HOST_APP
 from pyrevit import EXEC_PARAMS
 from pyrevit.userconfig import user_config
@@ -15,6 +17,13 @@ else:
 
 import PlatformSettings
 
+def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
+    if HOST_APP.language == LanguageType.Russian:
+        user_config.user_locale = 'ru'
+
+    if HOST_APP.language == LanguageType.English_USA:
+        user_config.user_locale = 'en_us'
+
 
 def open_platform_settings():
     settings = PlatformSettings.PlatformSettingsCommand()
@@ -25,7 +34,6 @@ def open_platform_settings():
 
         from pyrevit.loader.sessionmgr import execute_command
         execute_command("01dotbim-bim-установки-настройки-обновить")
-
 
 if __name__ == '__main__':
     open_platform_settings()
