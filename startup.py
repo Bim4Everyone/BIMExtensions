c76779c9f360f645bc0c263cd78d68feaa3e620e
dosymep <dosymep@gmail.com>
2022-03-11 16:49:06 +0300

    Добавил сервисы

--- startup.py
+++ startup.py
@@ -13,18 +13,32 @@ sys.path.append(bim4everyone_lib_path)
 import dosymep_libs
 dosymep_libs.load_assemblies()
 
+import devexpress_libs
+devexpress_libs.load_assemblies()
+
 import clr
 clr.AddReference('dosymep.Revit.dll')
 clr.AddReference('dosymep.Bim4Everyone.dll')
 
+clr.AddReference('dosymep.Xpf.Core.dll')
+clr.AddReference('dosymep.SimpleServices.dll')
+
+clr.AddReference('Serilog.dll')
+clr.AddReference('Serilog.Sinks.File.dll')
+
 from pyrevit import HOST_APP
 from pyrevit.userconfig import user_config
+
+from System.Windows import *
 
 from dosymep.Bim4Everyone.Schedules import SchedulesConfig
 from dosymep.Bim4Everyone.KeySchedules import KeySchedulesConfig
 from dosymep.Bim4Everyone.SystemParams import SystemParamsConfig
 from dosymep.Bim4Everyone.SharedParams import SharedParamsConfig
 from dosymep.Bim4Everyone.ProjectParams import ProjectParamsConfig
+from dosymep.Bim4Everyone.SimpleServices import ServicesProvider
+
+from dosymep.SimpleServices import INotificationService, ILoggerService
 
 
 def get_config_path(section, option):
@@ -45,5 +59,12 @@ def load_platform_settings():
     SharedParamsConfig.LoadInstance(shared_params_path)
     ProjectParamsConfig.LoadInstance(project_params_path)
 
+    ServicesProvider.LoadInstance(__revit__)
+
+    notification_service = ServicesProvider.GetPlatformService[INotificationService]()
+    notification_service.CreateNotification("Bim4Everyone", "Инициализация платформы прошла успешно.", None).ShowAsync()
+
+    logger = ServicesProvider.GetPlatformService[ILoggerService]()
+    logger.Debug("Инициализация платформы.")
 
 load_platform_settings()
