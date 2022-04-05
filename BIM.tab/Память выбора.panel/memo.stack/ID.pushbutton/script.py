9f15975435689023fed2f1643a3d7ab0ef3d1088
dosymep <dosymep@gmail.com>
2022-04-05 12:46:22 +0300

    Добавил логгирование команды ID

--- BIM.tab/Память выбора.panel/memo.stack/ID.pushbutton/script.py
+++ BIM.tab/Память выбора.panel/memo.stack/ID.pushbutton/script.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
+
 import clr
 clr.AddReference("PresentationCore")
 
-from System.Windows import *
-
-from Autodesk.Revit.DB import *
-from Autodesk.Revit.UI import *
 from Autodesk.Revit.UI.Selection import *
 from Autodesk.Revit.Exceptions import *
 
 from pyrevit import forms
+from dosymep_libs.bim4everyone import *
 
 document = __revit__.ActiveUIDocument.Document
 uiDocument = __revit__.ActiveUIDocument
 
-with forms.WarningBar(title="Выберите элемент связанного файла"):
-    try:
-        selectedElement = uiDocument.Selection.PickObject(ObjectType.LinkedElement, "Выберите элемент связанного файла.")
-        linkedDocuments = [ documentInstance.GetLinkDocument() for documentInstance in FilteredElementCollector(document).OfClass(RevitLinkInstance) if documentInstance.Id == selectedElement.ElementId ]
 
-        linkedDocument = next(iter(linkedDocuments), None)
-        if linkedDocument:
-            linkedElement = linkedDocument.GetElement(selectedElement.LinkedElementId)
+@log_plugin("ID")
+def script_execute(plugin_logger):
+    with forms.WarningBar(title="Выберите элемент связанного файла"):
+        try:
+            selectedElement = uiDocument.Selection.PickObject(ObjectType.LinkedElement, "Выберите элемент связанного файла.")
+            linkedDocuments = [ documentInstance.GetLinkDocument() for documentInstance in FilteredElementCollector(document).OfClass(RevitLinkInstance) if documentInstance.Id == selectedElement.ElementId ]
 
-            Clipboard.SetDataObject(linkedElement.Id.ToString())
-            print "{title}.rvt ID: {elementId}".format(title=linkedDocument.Title, elementId=linkedElement.Id)
-        else:
-            forms.alert("Не был найден элемент в связанных файлах.", title="Сообщение")
-    except OperationCanceledException:
-        pass
+            linkedDocument = next(iter(linkedDocuments), None)
+            if linkedDocument:
+                linkedElement = linkedDocument.GetElement(selectedElement.LinkedElementId)
+
+                Clipboard.SetDataObject(linkedElement.Id.ToString())
+                print "{title}.rvt ID: {elementId}".format(title=linkedDocument.Title, elementId=linkedElement.Id)
+            else:
+                forms.alert("Не был найден элемент в связанных файлах.", title="Сообщение")
+        except OperationCanceledException:
+            plugin_logger.Warning("Операция была отменена пользователем.")
+
+
+script_execute()
