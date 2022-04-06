c9b148537cd139a6d25a28a7b61ddc612d5344f1
dosymep <dosymep@gmail.com>
2022-04-06 14:51:45 +0300

    Добавил логгирование в скрипт копирования легенд в проект

--- BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Копировать легенды в проект.pushbutton/script.py
+++ BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Копировать легенды в проект.pushbutton/script.py
@@ -4,8 +4,15 @@ import os.path as op
 
 from pyrevit import forms
 from pyrevit import script
+from pyrevit import EXEC_PARAMS
 from pyrevit import revit, DB
 from pyrevit.framework import List, Controls
+
+
+
+from dosymep_libs.bim4everyone import *
+from dosymep_libs.simple_services import *
+
 
 doc = __revit__.ActiveUIDocument.Document
 app = __revit__.Application
@@ -119,82 +126,87 @@ class SelectLevelFrom(forms.TemplateUserInputWindow):
         self.Close()
 
 
-# find open documents other than the active doc
-open_docs = [d for d in revit.docs if not d.IsLinked]
-open_docs.remove(revit.doc)
-if len(open_docs) < 1:
-    forms.alert('Должно быть открыто минимум два проекта.', exitscript=True)
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    open_docs = [d for d in revit.docs if not d.IsLinked]
+    open_docs.remove(revit.doc)
+    if len(open_docs) < 1:
+        forms.alert('Должно быть открыто минимум два проекта.', exitscript=True)
 
-selection = [x for x in revit.get_selection()
-             if x.ViewType == DB.ViewType.Legend]
+    selection = [x for x in revit.get_selection()
+                 if x.ViewType == DB.ViewType.Legend]
 
-if not selection:
-    forms.alert("Должна быть выбрана как минимум одна легенда.", exitscript=True)
+    if not selection:
+        forms.alert("Должна быть выбрана как минимум одна легенда.", exitscript=True)
 
-o_docs = [doc_Option(doc) for doc in open_docs]
-res = SelectLevelFrom.show(o_docs,
-                           n_projects=len(o_docs),
-                           title='Выберите проекты',
-                           button_name='Копировать легенды')
+    o_docs = [doc_Option(doc) for doc in open_docs]
+    res = SelectLevelFrom.show(o_docs,
+                               n_projects=len(o_docs),
+                               title='Выберите проекты',
+                               button_name='Копировать легенды')
 
-if not res:
-    script.exit()
+    if not res:
+        script.exit()
 
-docs_2_process = [doc for doc in res['docs'] if doc.state]
-if not docs_2_process:
-    script.exit()
+    docs_2_process = [doc for doc in res['docs'] if doc.state]
+    if not docs_2_process:
+        script.exit()
 
-for dest_doc in docs_2_process:
-    legends_project = []
-    base_legend_view = None
+    for dest_doc in docs_2_process:
+        legends_project = []
+        base_legend_view = None
 
-    for v in DB.FilteredElementCollector(dest_doc.doc).OfClass(DB.View):
-        if v.ViewType == DB.ViewType.Legend:
-            legends_project.append(v.Name)
-            base_legend_view = v
+        for v in DB.FilteredElementCollector(dest_doc.doc).OfClass(DB.View):
+            if v.ViewType == DB.ViewType.Legend:
+                legends_project.append(v.Name)
+                base_legend_view = v
 
-    if base_legend_view is None:
-        forms.alert('В проекте "{0}" должна быть минимум одна легенда.'.format(dest_doc.Title), exitscript=True)
+        if base_legend_view is None:
+            forms.alert('В проекте "{0}" должна быть минимум одна легенда.'.format(dest_doc.Title), exitscript=True)
 
-    for srcView in selection:
-        viewElements = \
-            DB.FilteredElementCollector(revit.doc, srcView.Id) \
-                .ToElements()
+        for srcView in selection:
+            viewElements = \
+                DB.FilteredElementCollector(revit.doc, srcView.Id) \
+                    .ToElements()
 
-        element_list = []
-        for el in viewElements:
-            if isinstance(el, DB.Element) and el.Category:
-                element_list.append(el.Id)
+            element_list = []
+            for el in viewElements:
+                if isinstance(el, DB.Element) and el.Category:
+                    element_list.append(el.Id)
 
-        if len(element_list) < 1:
-            print('Проверка содержимого {0}. Элементы в легенде не найдены.'.format(srcView.Title))
-            continue
+            if len(element_list) < 1:
+                print('Проверка содержимого {0}. Элементы в легенде не найдены.'.format(srcView.Title))
+                continue
 
-        with revit.Transaction('Copy Legends to this document', doc=dest_doc.doc):
-            dest_view = dest_doc.doc.GetElement(base_legend_view.Duplicate(DB.ViewDuplicateOption.Duplicate))
+            with revit.Transaction('Copy Legends to this document', doc=dest_doc.doc):
+                dest_view = dest_doc.doc.GetElement(base_legend_view.Duplicate(DB.ViewDuplicateOption.Duplicate))
 
-            options = DB.CopyPasteOptions()
-            options.SetDuplicateTypeNamesHandler(CopyUseDestination())
-            copied_element = \
-                DB.ElementTransformUtils.CopyElements(
-                    srcView,
-                    List[DB.ElementId](element_list),
-                    dest_view,
-                    None,
-                    options)
+                options = DB.CopyPasteOptions()
+                options.SetDuplicateTypeNamesHandler(CopyUseDestination())
+                copied_element = \
+                    DB.ElementTransformUtils.CopyElements(
+                        srcView,
+                        List[DB.ElementId](element_list),
+                        dest_view,
+                        None,
+                        options)
 
-            for dest, src in zip(copied_element, element_list):
-                dest_view.SetElementOverrides(dest, srcView.GetElementOverrides(src))
+                for dest, src in zip(copied_element, element_list):
+                    dest_view.SetElementOverrides(dest, srcView.GetElementOverrides(src))
 
-            if srcView.Name not in legends_project:
-                dest_view.Name = srcView.Name
-                legends_project.append(dest_view.Name)
-            else:
-                index = 1
-                while '{}-{}'.format(srcView.Name, index) in legends_project:
-                    index += 1
-                dest_view.Name = '{}-{}'.format(srcView.Name, index)
-                legends_project.append(dest_view.Name)
-            dest_view.Scale = srcView.Scale
+                if srcView.Name not in legends_project:
+                    dest_view.Name = srcView.Name
+                    legends_project.append(dest_view.Name)
+                else:
+                    index = 1
+                    while '{}-{}'.format(srcView.Name, index) in legends_project:
+                        index += 1
+                    dest_view.Name = '{}-{}'.format(srcView.Name, index)
+                    legends_project.append(dest_view.Name)
+                dest_view.Scale = srcView.Scale
 
-forms.alert("Готово!")
+
+    show_executed_script_notification()
+
+
+script_execute()
