1115b6119efc76c4a6ff965926f4433357c08b1f
dosymep <dosymep@gmail.com>
2022-04-06 15:43:59 +0300

    Добавил логгирование в панель "Виды"

--- BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Преобразовать легенду в чертежный вид.pushbutton/script.py
+++ BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Преобразовать легенду в чертежный вид.pushbutton/script.py
@@ -15,17 +15,20 @@ from Autodesk.Revit.DB import *
 from pyrevit import forms
 from pyrevit import script
 from pyrevit import revit, DB
+from pyrevit import EXEC_PARAMS
 from pyrevit.framework import List
 
 from dosymep.Bim4Everyone.Templates import ProjectParameters
 from dosymep.Bim4Everyone.ProjectParams import ProjectParamsConfig
 
+from dosymep_libs.bim4everyone import *
 
 doc = __revit__.ActiveUIDocument.Document
 app = __revit__.Application
 
 project_params = ProjectParameters.Create(app)
 project_params.SetupRevitParams(doc, ProjectParamsConfig.Instance.ViewGroup)
+
 
 class SelectLevelFrom(forms.TemplateUserInputWindow):
     xaml_source = op.join(op.dirname(__file__), 'SelectFromComboBox.xaml')
@@ -55,84 +58,89 @@ class CopyUseDestination(DB.IDuplicateTypeNamesHandler):
         return DB.DuplicateTypeAction.UseDestinationTypes
 
 
-selection = [x for x in revit.get_selection()
-             if x.ViewType == DB.ViewType.Legend]
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    selection = [x for x in revit.get_selection()
+                 if x.ViewType == DB.ViewType.Legend]
 
-if not selection:
-    forms.alert('Должна быть выбрана минимум одна легенда.', exitscript=True)
+    if not selection:
+        forms.alert("Должна быть выбрана минимум одна легенда.", exitscript=True)
 
-poject_drafts = []
-doc_views = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views).ToElements()
+    poject_drafts = []
+    doc_views = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Views).ToElements()
 
-views = []
-for view in doc_views:
-    view_group = view.GetParamValueOrDefault(ProjectParamsConfig.Instance.ViewGroup)
-    if view_group and view_group not in views:
-        views.append(view_group)
+    views = []
+    for view in doc_views:
+        view_group = view.GetParamValueOrDefault(ProjectParamsConfig.Instance.ViewGroup)
+        if view_group and view_group not in views:
+            views.append(view_group)
 
-response = SelectLevelFrom.show([],
-                                title="Выберите назначение вида",
-                                button_name="Ок",
-                                views=views)
+    response = SelectLevelFrom.show([],
+                                    title="Выберите назначение вида",
+                                    button_name="Ок",
+                                    views=views)
 
-if not response:
-    script.exit()
+    if not response:
+        script.exit()
 
-drafting_view = False
-for v in DB.FilteredElementCollector(revit.doc).OfClass(DB.View):
-    if v.ViewType == DB.ViewType.DraftingView:
-        drafting_view = True
-        draftingViewType = v
-        poject_drafts.append(v.Name)
+    drafting_view = False
+    for v in DB.FilteredElementCollector(revit.doc).OfClass(DB.View):
+        if v.ViewType == DB.ViewType.DraftingView:
+            drafting_view = True
+            draftingViewType = v
+            poject_drafts.append(v.Name)
 
-if not drafting_view:
-    forms.alert('В проекте должен быть минимум один чертежный вид.', exitscript=True)
+    if not drafting_view:
+        forms.alert('В проекте должен быть минимум один чертежный вид.', exitscript=True)
 
-for src_view in selection:
-    view_elements = DB.FilteredElementCollector(revit.doc, src_view.Id) \
-        .ToElements()
+    for src_view in selection:
+        view_elements = DB.FilteredElementCollector(revit.doc, src_view.Id) \
+            .ToElements()
 
-    element_list = []
-    for el in view_elements:
-        if isinstance(el, DB.Element) \
-                and el.Category \
-                and el.Category.Id != ElementId(BuiltInCategory.OST_LegendComponents):
-            element_list.append(el.Id)
+        element_list = []
+        for el in view_elements:
+            if isinstance(el, DB.Element) \
+                    and el.Category \
+                    and el.Category.Id != ElementId(BuiltInCategory.OST_LegendComponents):
+                element_list.append(el.Id)
 
-    if len(element_list) < 1:
-        print('Пропуск {0}. Никаких элементов не найдено.'.format(src_view.Name))
-        continue
+        if len(element_list) < 1:
+            print('Пропуск {0}. Никаких элементов не найдено.'.format(src_view.Name))
+            continue
 
-    with revit.Transaction('Duplicate Legend as Drafting'):
-        dest_view = revit.doc.GetElement(draftingViewType.Duplicate(DB.ViewDuplicateOption.Duplicate))
+        with revit.Transaction('Duplicate Legend as Drafting'):
+            dest_view = revit.doc.GetElement(draftingViewType.Duplicate(DB.ViewDuplicateOption.Duplicate))
 
-        options = DB.CopyPasteOptions()
-        options.SetDuplicateTypeNamesHandler(CopyUseDestination())
+            options = DB.CopyPasteOptions()
+            options.SetDuplicateTypeNamesHandler(CopyUseDestination())
 
-        copiedElement = \
-            DB.ElementTransformUtils.CopyElements(
-                src_view,
-                List[DB.ElementId](element_list),
-                dest_view,
-                None,
-                options)
+            copiedElement = \
+                DB.ElementTransformUtils.CopyElements(
+                    src_view,
+                    List[DB.ElementId](element_list),
+                    dest_view,
+                    None,
+                    options)
 
-        for dest, src in zip(copiedElement, element_list):
-            dest_view.SetElementOverrides(dest, src_view.GetElementOverrides(src))
+            for dest, src in zip(copiedElement, element_list):
+                dest_view.SetElementOverrides(dest, src_view.GetElementOverrides(src))
 
-        if src_view.Name not in poject_drafts:
-            dest_view.Name = src_view.Name
-            poject_drafts.append(dest_view.Name)
-        else:
-            index = 1
-            while '{}-{}'.format(src_view.Name, index) in poject_drafts:
-                index += 1
-            dest_view.Name = '{}-{}'.format(src_view.Name, index)
-            poject_drafts.append(dest_view.Name)
+            if src_view.Name not in poject_drafts:
+                dest_view.Name = src_view.Name
+                poject_drafts.append(dest_view.Name)
+            else:
+                index = 1
+                while '{}-{}'.format(src_view.Name, index) in poject_drafts:
+                    index += 1
+                dest_view.Name = '{}-{}'.format(src_view.Name, index)
+                poject_drafts.append(dest_view.Name)
 
-        if response:
-            purpose = response["purpose"]
-            dest_view.Scale = src_view.Scale
-            dest_view.SetParamValue(ProjectParamsConfig.Instance.ViewGroup, purpose)
+            if response:
+                purpose = response["purpose"]
+                dest_view.Scale = src_view.Scale
+                dest_view.SetParamValue(ProjectParamsConfig.Instance.ViewGroup, purpose)
 
-forms.alert('Готово!')
+    show_executed_script_notification()
+
+
+script_execute()
--- BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Преобразовать чертежный вид в легенду.pushbutton/script.py
+++ BIM.tab/Виды.panel/Виды.stack/Другие команды.pulldown/Преобразовать чертежный вид в легенду.pushbutton/script.py
@@ -3,7 +3,10 @@
 from pyrevit import forms
 from pyrevit import script
 from pyrevit import revit, DB
+from pyrevit import EXEC_PARAMS
 from pyrevit.framework import List
+
+from dosymep_libs.bim4everyone import *
 
 doc = __revit__.ActiveUIDocument.Document
 app = __revit__.Application
@@ -16,64 +19,69 @@ class CopyUseDestination(DB.IDuplicateTypeNamesHandler):
         return DB.DuplicateTypeAction.UseDestinationTypes
 
 
-# get a list of selected drafting views
-selection = [el for el in revit.get_selection()
-             if isinstance(el, DB.View)
-             and el.ViewType == DB.ViewType.DraftingView]
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    # get a list of selected drafting views
+    selection = [el for el in revit.get_selection()
+                 if isinstance(el, DB.View)
+                 and el.ViewType == DB.ViewType.DraftingView]
 
-if not selection:
-    forms.alert('Должен быть выбран как минимум один чертежный вид.', exitscript=True)
+    if not selection:
+        forms.alert('Должен быть выбран как минимум один чертежный вид.', exitscript=True)
 
-legends_project = []
-base_legend_view = None
-for v in DB.FilteredElementCollector(revit.doc).OfClass(DB.View):
-    if v.ViewType == DB.ViewType.Legend:
-        base_legend_view = v
-        legends_project.append(v.Name)
+    legends_project = []
+    base_legend_view = None
+    for v in DB.FilteredElementCollector(revit.doc).OfClass(DB.View):
+        if v.ViewType == DB.ViewType.Legend:
+            base_legend_view = v
+            legends_project.append(v.Name)
 
-if not base_legend_view:
-    forms.alert('В модели должна быть как минимум одна легенда.', exitscript=True)
+    if not base_legend_view:
+        forms.alert('В модели должна быть как минимум одна легенда.', exitscript=True)
 
-for srcView in selection:
-    view_elements = \
-        DB.FilteredElementCollector(revit.doc, srcView.Id).ToElements()
+    for srcView in selection:
+        view_elements = \
+            DB.FilteredElementCollector(revit.doc, srcView.Id).ToElements()
 
-    elements_to_copy = []
-    for el in view_elements:
-        if isinstance(el, DB.Element) and el.Category:
-            elements_to_copy.append(el.Id)
+        elements_to_copy = []
+        for el in view_elements:
+            if isinstance(el, DB.Element) and el.Category:
+                elements_to_copy.append(el.Id)
 
-    if len(elements_to_copy) < 1:
-        logger.debug('Пропуск {0}. Никаких копируемых элементов не найдено.'.format(srcView.ViewName))
-        continue
+        if len(elements_to_copy) < 1:
+            logger.debug('Пропуск {0}. Никаких копируемых элементов не найдено.'.format(srcView.ViewName))
+            continue
 
-    with revit.Transaction('Duplicate Drafting as Legend'):
-        dest_view = revit.doc.GetElement(base_legend_view.Duplicate(DB.ViewDuplicateOption.Duplicate))
+        with revit.Transaction('Duplicate Drafting as Legend'):
+            dest_view = revit.doc.GetElement(base_legend_view.Duplicate(DB.ViewDuplicateOption.Duplicate))
 
-        options = DB.CopyPasteOptions()
-        options.SetDuplicateTypeNamesHandler(CopyUseDestination())
-        copied_element = \
-            DB.ElementTransformUtils.CopyElements(
-                srcView,
-                List[DB.ElementId](elements_to_copy),
-                dest_view,
-                None,
-                options)
+            options = DB.CopyPasteOptions()
+            options.SetDuplicateTypeNamesHandler(CopyUseDestination())
+            copied_element = \
+                DB.ElementTransformUtils.CopyElements(
+                    srcView,
+                    List[DB.ElementId](elements_to_copy),
+                    dest_view,
+                    None,
+                    options)
 
-        for dest, src in zip(copied_element, elements_to_copy):
-            dest_view.SetElementOverrides(dest,
-                                          srcView.GetElementOverrides(src))
+            for dest, src in zip(copied_element, elements_to_copy):
+                dest_view.SetElementOverrides(dest,
+                                              srcView.GetElementOverrides(src))
 
-        if srcView.Name not in legends_project:
-            dest_view.Name = srcView.Name
-            legends_project.append(dest_view.Name)
-        else:
-            index = 1
-            while '{}-{}'.format(srcView.Name, index) in legends_project:
-                index += 1
-            dest_view.Name = '{}-{}'.format(srcView.Name, index)
-            legends_project.append(dest_view.Name)
+            if srcView.Name not in legends_project:
+                dest_view.Name = srcView.Name
+                legends_project.append(dest_view.Name)
+            else:
+                index = 1
+                while '{}-{}'.format(srcView.Name, index) in legends_project:
+                    index += 1
+                dest_view.Name = '{}-{}'.format(srcView.Name, index)
+                legends_project.append(dest_view.Name)
 
-        dest_view.Scale = srcView.Scale
+            dest_view.Scale = srcView.Scale
 
-forms.alert("Готово!")
+    show_executed_script_notification()
+
+
+script_execute()
