412797ccf3fcd8c3c4a1fcde17f9db1533c6e840
dosymep <dosymep@gmail.com>
2022-06-02 19:09:38 +0300

    Добавил уведомления для скрипта Выгрузить связанные файлы

--- BIM.tab/Связанные файлы.panel/связи.stack/Выгрузить связанные файлы.pushbutton/script.py
+++ BIM.tab/Связанные файлы.panel/связи.stack/Выгрузить связанные файлы.pushbutton/script.py
@@ -9,12 +9,14 @@ from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
 
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-    selected_files = forms.pick_file(files_filter="Revit files (*.rvt)|*.rvt", multi_file=True, title="Выберите Revit файлы")
-    if selected_files:
-        DocumentExtensions.UnloadAllLinks(selected_files)
-        show_executed_script_notification()
+    selected_files = forms.pick_file(files_filter="Revit (*.rvt)|*.rvt", multi_file=True, title="Выберите Revit файлы")
+    if not selected_files:
+        script.exit()
+
+    DocumentExtensions.UnloadAllLinks(selected_files)
 
 
 script_execute()
