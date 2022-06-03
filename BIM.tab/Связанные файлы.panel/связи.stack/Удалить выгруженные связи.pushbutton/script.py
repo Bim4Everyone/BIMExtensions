9a324eb43d15fbbd798d43442aaa2fbf1141474e
dosymep <dosymep@gmail.com>
2022-06-03 11:49:51 +0300

    Обновил скрипт удаления выгруженных связей на использование уведомлений

--- BIM.tab/Связанные файлы.panel/связи.stack/Удалить выгруженные связи.pushbutton/script.py
+++ BIM.tab/Связанные файлы.panel/связи.stack/Удалить выгруженные связи.pushbutton/script.py
@@ -5,35 +5,37 @@ clr.AddReference("System")
 from System.Collections.Generic import *
 from Autodesk.Revit.DB import *
 from pyrevit import forms
+from pyrevit import script
+from pyrevit import revit
 from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
 
+
+document = __revit__.ActiveUIDocument.Document
+
+
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-    document = __revit__.ActiveUIDocument.Document
 
-    linkedFiles = FilteredElementCollector(document)\
+    linked_files = FilteredElementCollector(document)\
         .OfCategory(BuiltInCategory.OST_RvtLinks)\
         .WhereElementIsElementType()\
         .ToElementIds()
 
-    linkedFiles = [ item for item in linkedFiles if RevitLinkType.IsLoaded(document, item) == False ]
-    linkedFileNames = [ document.GetElement(item) for item in linkedFiles]
-    linkedFileNames = [ item.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString() for item in linkedFileNames ]
+    linked_files = [ item for item in linked_files if RevitLinkType.IsLoaded(document, item) == False ]
+    linked_file_names = [ document.GetElement(item) for item in linked_files]
+    linked_file_names = [ item.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString() for item in linked_file_names ]
 
-    if linkedFileNames:
-        linkedFileNames.sort()
-        result = forms.alert("Будут удалены следующие связанные файлы:\r\n - " + "\r\n - ".join(linkedFileNames), title="Предупреждение", ok=False, yes=True, no=True)
+    if linked_file_names:
+        linked_file_names.sort()
+        result = forms.alert("Будут удалены следующие связанные файлы:\r\n - " + "\r\n - ".join(linked_file_names), title="Предупреждение", ok=False, yes=True, no=True)
+        if not result:
+            script.exit()
 
-        if result:
-            with Transaction(document, "Удаление связанных файлов") as transaction:
-                transaction.Start()
+        with revit.Transaction("BIM: Удаление связанных файлов"):
+            document.Delete(List[ElementId](linked_files))
 
-                document.Delete(List[ElementId](linkedFiles))
-
-                transaction.Commit()
-
-            show_executed_script_notification()
 
 script_execute()
