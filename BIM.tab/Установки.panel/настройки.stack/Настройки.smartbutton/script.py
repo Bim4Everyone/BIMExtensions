fb5e03ce5acbe878b01c27303095f588dca4878c
dosymep <dosymep@gmail.com>
2023-04-21 14:31:38 +0300

    Обновил вызов плагина настроек

--- BIM.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
+++ BIM.tab/Установки.panel/настройки.stack/Настройки.smartbutton/script.py
@@ -4,20 +4,20 @@ import clr
 clr.AddReference('dosymep.Revit.dll')
 clr.AddReference('dosymep.Bim4Everyone.dll')
 
+from System import InvalidOperationException, OperationCanceledException
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
 from Autodesk.Revit.ApplicationServices import LanguageType
 
 from pyrevit import HOST_APP
 from pyrevit import EXEC_PARAMS
 from pyrevit.userconfig import user_config
 
+from dosymep.Revit import *
+from dosymep.Bim4Everyone import *
+
 from dosymep_libs.bim4everyone import *
-
-if HOST_APP.version == "2020":
-    clr.AddReference("PlatformSettings.dll")
-else:
-    clr.AddReference("PlatformSettings_{}.dll".format(HOST_APP.version))
-
-import PlatformSettings
 
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
     user_config.auto_update = True
@@ -33,14 +33,14 @@ def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
 
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-    settings = PlatformSettings.PlatformSettingsCommand()
-    result = settings.OpenSettingsWindow(__revit__)
+    try:
+        invoke_command(PlatformCommandIds.PlatformSettingsCommandId)
 
-    if result:
         user_config.reload()
-
         from pyrevit.loader.sessionmgr import execute_command
         execute_command("01dotbim-bim-установки-настройки-обновить")
+    except OperationCanceledException:
+        show_canceled_script_notification()
 
 
 if __name__ == '__main__':
