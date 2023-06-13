b2cfa6ae243b675eba7f16b95a3df67ac0382dab
dosymep <dosymep@gmail.com>
2023-06-13 10:39:10 +0300

    Обновил вывод логов в инициализации платформы

--- hooks/app-init.py
+++ hooks/app-init.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import LibGit2Sharp as libgit
-from pyrevit.versionmgr import updater
+from System.Collections.Generic import Dictionary
 
+from pyrevit.versionmgr import updater
 from dosymep_libs.simple_services import *
 
 logger = get_logger_service()
-logger.Debug("Инициализация платформы.")
+
+
+def to_dictionary(repo_info):
+    return Dictionary[str, object]({
+        "name": repo_info.name,
+        "branch": repo_info.branch,
+        "directory": repo_info.directory,
+        "head_name": repo_info.head_name,
+        "last_commit_hash": repo_info.last_commit_hash,
+    })
+
+
+def log_trace(message):
+    logger.Information(message)
+    for repo_info in updater.get_all_extension_repos():
+        logger.Information("Информация расширения: \"{@RepoInfo}\"", to_dictionary(repo_info))
 
 
 def check_updates():
@@ -22,14 +38,16 @@ def check_updates():
                 updater.update_repo(repo_info)
         except Exception as ex:
             status_update = True
-            logger.Debug(ex, "Ошибка обновления расширения: \"{}\".".format(repo_info))
+            logger.Warning(ex, "Ошибка обновления расширения: \"{@RepoInfo}\"", to_dictionary(repo_info))
 
     return status_update
 
 
 if check_updates():
+    log_trace("Инициализация платформы прошла с ошибкой")
     show_notification_service("Bim4Everyone",
                               "Инициализация платформы прошла с ошибкой, пожалуйста переустановите платформу.")
 else:
+    log_trace("Инициализация платформы прошла успешно")
     show_notification_service("Bim4Everyone",
                               "Инициализация платформы прошла успешно.")
