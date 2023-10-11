b5ce85add2f0e9447caa15f44f5d929f15fa90e3
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-11 11:53:42 +0300

    Переименовать Очистить в Обнулить и включить поддержку до 2024

--- BIM.tab/Память выбора.panel/Память выбора 2.stack/Обнулить.pushbutton/script.py
+++ BIM.tab/Память выбора.panel/Память выбора 2.stack/Обнулить.pushbutton/script.py
@@ -1,13 +1,11 @@
-import os
-import os.path as op
 import pickle as pl
 
 from pyrevit import script
 from pyrevit import EXEC_PARAMS
+from dosymep_libs.bim4everyone import *
 
-from dosymep_libs.bim4everyone import log_plugin
 
-
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
     datafile = script.get_document_data_file("SelList", "pym")
