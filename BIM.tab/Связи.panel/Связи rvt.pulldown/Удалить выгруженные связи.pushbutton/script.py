b891c32ac9eaeb0a4e34f1d945d967ac5521da42
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-11 15:41:32 +0300

    Включить поддержку 2024 версии для "Удалить выгруженные связи"

--- BIM.tab/Связи.panel/Связи rvt.pulldown/Удалить выгруженные связи.pushbutton/script.py
+++ BIM.tab/Связи.panel/Связи rvt.pulldown/Удалить выгруженные связи.pushbutton/script.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import clr
+
 clr.AddReference("System")
 
 from System.Collections.Generic import *
@@ -11,26 +12,26 @@ from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
 
-
 document = __revit__.ActiveUIDocument.Document
 
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-
-    linked_files = FilteredElementCollector(document)\
-        .OfCategory(BuiltInCategory.OST_RvtLinks)\
-        .WhereElementIsElementType()\
+    linked_files = FilteredElementCollector(document) \
+        .OfCategory(BuiltInCategory.OST_RvtLinks) \
+        .WhereElementIsElementType() \
         .ToElementIds()
 
-    linked_files = [ item for item in linked_files if RevitLinkType.IsLoaded(document, item) == False ]
-    linked_file_names = [ document.GetElement(item) for item in linked_files]
-    linked_file_names = [ item.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString() for item in linked_file_names ]
+    linked_files = [item for item in linked_files if RevitLinkType.IsLoaded(document, item) == False]
+    linked_file_names = [document.GetElement(item) for item in linked_files]
+    linked_file_names = [item.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString() for item in
+                         linked_file_names]
 
     if linked_file_names:
         linked_file_names.sort()
-        result = forms.alert("Будут удалены следующие связанные файлы:\r\n - " + "\r\n - ".join(linked_file_names), title="Предупреждение", ok=False, yes=True, no=True)
+        result = forms.alert("Будут удалены следующие связанные файлы:\r\n - " + "\r\n - ".join(linked_file_names),
+                             title="Предупреждение", ok=False, yes=True, no=True)
         if not result:
             script.exit()
 
