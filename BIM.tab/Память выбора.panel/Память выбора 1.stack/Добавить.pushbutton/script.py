e3da36e8eb237ff831cc58f5dad143a043618ba9
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-11 11:54:27 +0300

    Включить поддержку 2024 версии для "Добавить"

--- BIM.tab/Память выбора.panel/Память выбора 1.stack/Добавить.pushbutton/script.py
+++ BIM.tab/Память выбора.panel/Память выбора 1.stack/Добавить.pushbutton/script.py
@@ -1,18 +1,25 @@
 import pickle
+import clr
 
-from pyrevit import script
+clr.AddReference("dosymep.Revit.dll")
+
 from pyrevit import revit
 from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
 
+import dosymep
 
+clr.ImportExtensions(dosymep.Revit)
+
+
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
     datafile = script.get_document_data_file("SelList", "pym")
 
     selection = revit.get_selection()
-    selected_ids = {str(elid.IntegerValue) for elid in selection.element_ids}
+    selected_ids = {str(elid.GetIdValue()) for elid in selection.element_ids}
 
     try:
         f = open(datafile, 'r')
