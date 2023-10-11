0117ae9e9e70ecb0fc5fba1ff083391d4cc734eb
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-11 11:54:39 +0300

    Включить поддержку 2024 версии для "Удалить"

--- BIM.tab/Память выбора.panel/Память выбора 1.stack/Удалить.pushbutton/script.py
+++ BIM.tab/Память выбора.panel/Память выбора 1.stack/Удалить.pushbutton/script.py
@@ -1,6 +1,9 @@
 import os
 import os.path as op
 import pickle as pl
+import clr
+
+clr.AddReference("dosymep.Revit.dll")
 
 from pyrevit import revit
 from pyrevit import script
@@ -8,13 +11,18 @@ from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
 
+import dosymep
 
+clr.ImportExtensions(dosymep.Revit)
+
+
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
     datafile = script.get_document_data_file("SelList", "pym")
 
     selection = revit.get_selection()
-    selected_ids = {str(elid.IntegerValue) for elid in selection.element_ids}
+    selected_ids = {str(elid.GetIdValue()) for elid in selection.element_ids}
 
     try:
         f = open(datafile, 'r')
