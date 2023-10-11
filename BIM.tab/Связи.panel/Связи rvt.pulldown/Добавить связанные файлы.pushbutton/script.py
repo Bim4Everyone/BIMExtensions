720c89d8167a86df27b808a50ab5020c0aa18eb8
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-11 15:42:17 +0300

    Включить поддержку 2024 версии для "Добавить связанные файлы"

--- BIM.tab/Связи.panel/Связи rvt.pulldown/Добавить связанные файлы.pushbutton/script.py
+++ BIM.tab/Связи.panel/Связи rvt.pulldown/Добавить связанные файлы.pushbutton/script.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import clr
+
 clr.AddReference("dosymep.Revit.dll")
 clr.AddReference("dosymep.Bim4Everyone.dll")
 
 import dosymep
+
 clr.ImportExtensions(dosymep.Revit)
 clr.ImportExtensions(dosymep.Bim4Everyone)
 
@@ -22,12 +24,13 @@ document = __revit__.ActiveUIDocument.Document
 
 
 def reload_existing_links(selected_files, progress, cancellation_token):
-    linked_files = FilteredElementCollector(document)\
-        .OfCategory(BuiltInCategory.OST_RvtLinks)\
-        .WhereElementIsElementType()\
+    linked_files = FilteredElementCollector(document) \
+        .OfCategory(BuiltInCategory.OST_RvtLinks) \
+        .WhereElementIsElementType() \
         .ToElements()
 
-    link_files = [(link_file, link_file.GetParamValue(BuiltInParameter.ALL_MODEL_TYPE_NAME)) for link_file in linked_files]
+    link_files = [(link_file, link_file.GetParamValue(BuiltInParameter.ALL_MODEL_TYPE_NAME)) for link_file in
+                  linked_files]
     link_files = [(link_file, selected_files.pop(link_name.lower(), None)) for (link_file, link_name) in link_files]
     link_files = [(link_file, link_file_path) for link_file, link_file_path in link_files if link_file_path]
 
@@ -38,7 +41,7 @@ def reload_existing_links(selected_files, progress, cancellation_token):
 
         link_file_model_path = ModelPathUtils.ConvertUserVisiblePathToModelPath(link_file_path)
         link_file.LoadFrom(link_file_model_path, WorksetConfiguration())
-            
+
 
 def create_revit_links(selected_files, progress, cancellation_token):
     error_list = []
@@ -56,7 +59,8 @@ def create_revit_links(selected_files, progress, cancellation_token):
                 link_options = RevitLinkOptions(True)
                 link_load_result = RevitLinkType.Create(document, link_file, link_options)
 
-                revit_link_instance = RevitLinkInstance.Create(document, link_load_result.ElementId, ImportPlacement.Shared)
+                revit_link_instance = RevitLinkInstance.Create(document, link_load_result.ElementId,
+                                                               ImportPlacement.Shared)
 
                 revit_link_type = document.GetElement(revit_link_instance.GetTypeId())
                 revit_link_type.Parameter[BuiltInParameter.WALL_ATTR_ROOM_BOUNDING].Set(1)
@@ -70,20 +74,21 @@ def create_revit_links(selected_files, progress, cancellation_token):
         error_message = "Файлы с другой системой координат:\r\n - " + "\r\n - ".join(error_list)
 
     if error_version_list:
-        error_message = error_message if error_message else "" + "\r\nФайлы с созданные в другой версии Revit:\r\n - " + "\r\n - ".join(error_version_list)
+        error_message = error_message if error_message else "" + "\r\nФайлы с созданные в другой версии Revit:\r\n - " + "\r\n - ".join(
+            error_version_list)
 
     return error_message
-
 
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-    selected_files = forms.pick_file(files_filter="Revit files (*.rvt)|*.rvt", multi_file=True, title="Выберите Revit файлы")
+    selected_files = forms.pick_file(files_filter="Revit files (*.rvt)|*.rvt", multi_file=True,
+                                     title="Выберите Revit файлы")
     if not selected_files:
         script.exit()
 
-    selected_file_names = [ Path.GetFileName(value).lower() for value in list(selected_files) ]
+    selected_file_names = [Path.GetFileName(value).lower() for value in list(selected_files)]
     selected_files = dict(zip(selected_file_names, selected_files))
 
     error_message = None
