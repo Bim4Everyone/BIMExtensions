eef72398a74cc996a39ee352a0cb9199b4b78e94
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-12 11:14:10 +0300

    Включить поддержку 2024 версии для "Сохранить семейство"

--- BIM.tab/Прочее.panel/Прочее 1.stack/Сохранить семейство.pushbutton/script.py
+++ BIM.tab/Прочее.panel/Прочее 1.stack/Сохранить семейство.pushbutton/script.py
@@ -19,39 +19,40 @@ temp_file_path = op.join(tempfile.gettempdir(), file_name)
 
 
 def remove_backups():
-	# удаляем свой бекап файла ревита
-	os.remove(temp_file_path)
+    # удаляем свой бекап файла ревита
+    os.remove(temp_file_path)
 
-	# удаляем бекапы созданные ревитом
-	dir_name = op.dirname(file_path)
-	backup_files = [op.join(dir_name, f) for f in os.listdir(dir_name)
-					if f.startswith(document.Title) and f != file_name and re.findall(r".*\.\d\d\d\d\..*", f)]
-	backup_files = [f for f in backup_files if op.isfile(f) ]
-	for backup_file in backup_files:
-		os.remove(backup_file)
+    # удаляем бекапы созданные ревитом
+    dir_name = op.dirname(file_path)
+    backup_files = [op.join(dir_name, f) for f in os.listdir(dir_name)
+                    if f.startswith(document.Title) and f != file_name and re.findall(r".*\.\d\d\d\d\..*", f)]
+    backup_files = [f for f in backup_files if op.isfile(f)]
+    for backup_file in backup_files:
+        os.remove(backup_file)
 
 
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-	if not document.IsFamilyDocument:
-		show_script_warning_notification("Сохранять разрешено только файлы семейств", exit_script=True)
+    if not document.IsFamilyDocument:
+        show_script_warning_notification("Сохранять разрешено только файлы семейств", exit_script=True)
 
-	try:
-		save_options = SaveAsOptions()
-		save_options.Compact = False
-		save_options.OverwriteExistingFile = True
+    try:
+        save_options = SaveAsOptions()
+        save_options.Compact = False
+        save_options.OverwriteExistingFile = True
 
-		document.Save()
-		document.SaveAs(temp_file_path, save_options)
-		try:
-			document.SaveAs(file_path, save_options)
-		finally:
-			remove_backups()
-	except:
-		show_fatal_script_notification()
-		raise
+        document.Save()
+        document.SaveAs(temp_file_path, save_options)
+        try:
+            document.SaveAs(file_path, save_options)
+        finally:
+            remove_backups()
+    except:
+        show_fatal_script_notification()
+        raise
 
-	show_executed_script_notification()
+    show_executed_script_notification()
 
 
 script_execute()
