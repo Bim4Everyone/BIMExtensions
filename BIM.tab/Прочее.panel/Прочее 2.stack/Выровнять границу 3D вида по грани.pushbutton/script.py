cc3095a2620341864c3bb3f84e775690f704e430
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-13 10:30:50 +0300

    Включить поддержку 2024 версии для "Выровнять границу 3D вида по грани"

--- BIM.tab/Прочее.panel/Прочее 2.stack/Выровнять границу 3D вида по грани.pushbutton/script.py
+++ BIM.tab/Прочее.panel/Прочее 2.stack/Выровнять границу 3D вида по грани.pushbutton/script.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 """Aligns the section box of the current 3D view to selected face."""
+import math
 
 from pyrevit.framework import Math
 from pyrevit import revit, DB, UI
@@ -7,41 +8,39 @@ from pyrevit import forms
 from pyrevit import EXEC_PARAMS
 
 from dosymep_libs.bim4everyone import *
+from dosymep_libs.simple_services import *
 
-curview = revit.active_view
-
-
-def orientsectionbox(view):
-        try:
-            face = revit.pick_face()
-            box = view.GetSectionBox()
-            norm = face.ComputeNormal(DB.UV(0, 0)).Normalize()
-            boxNormal = box.Transform.Basis[0].Normalize()
-            angle = norm.AngleTo(boxNormal)
-            axis = DB.XYZ(0, 0, 1.0)
-            origin = DB.XYZ(box.Min.X + (box.Max.X - box.Min.X) / 2,
-                            box.Min.Y + (box.Max.Y - box.Min.Y) / 2, 0.0)
-            if norm.Y * boxNormal.X < 0:
-                rotate = \
-                    DB.Transform.CreateRotationAtPoint(axis, Math.PI / 2 - angle,
-                                                       origin)
-            else:
-                rotate = DB.Transform.CreateRotationAtPoint(axis,
-                                                            angle,
-                                                            origin)
-            box.Transform = box.Transform.Multiply(rotate)
-            with revit.Transaction('Orient Section Box to Face'):
-                view.SetSectionBox(box)
-                revit.uidoc.RefreshActiveView()
-        except Exception:
-            pass
+cur_view = revit.active_view
 
 
+def orient_section_box(view):
+    face = revit.pick_face()
+    if face is None:
+        raise OperationCanceledException()
+    try:
+        box = view.GetSectionBox()
+        face_norm = face.ComputeNormal(DB.UV(0, 0)).Normalize()
+        face_angle = DB.XYZ.BasisX.AngleOnPlaneTo(face_norm, DB.XYZ.BasisZ)
+        transform = box.Transform
+        box_angle = transform.BasisX.AngleOnPlaneTo(transform.OfVector(transform.BasisX), transform.BasisZ)
+        axis = DB.XYZ.BasisZ
+        origin = DB.XYZ((box.Max.X + box.Min.X) / 2,
+                        (box.Max.Y + box.Min.Y) / 2, 0.0)
+        rotation_total = DB.Transform.CreateRotationAtPoint(axis, face_angle - box_angle, origin)
+        box.Transform = box.Transform.Multiply(rotation_total)
+        with revit.Transaction('Orient Section Box to Face'):
+            view.SetSectionBox(box)
+            revit.uidoc.RefreshActiveView()
+    except Exception:
+        pass
+
+
+@notification()
 @log_plugin(EXEC_PARAMS.command_name)
 def script_execute(plugin_logger):
-    if isinstance(curview, DB.View3D) and curview.IsSectionBoxActive:
-        orientsectionbox(curview)
-    elif isinstance(curview, DB.View3D) and not curview.IsSectionBoxActive:
+    if isinstance(cur_view, DB.View3D) and cur_view.IsSectionBoxActive:
+        orient_section_box(cur_view)
+    elif isinstance(cur_view, DB.View3D) and not cur_view.IsSectionBoxActive:
         forms.alert("Границы 3D вида не включены.")
     else:
         forms.alert('Должен быть открыт 3D вид.')
