688fa34c8478bcb22002e02428591b5fe4847fbe
Vladimir Stroganov <vlastroganov@gmail.com>
2024-04-17 09:18:36 +0300

    Настройки: Сделано обновление сессии без обновления репозиториев (#18)

--- BIM.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
+++ BIM.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
@@ -4,14 +4,13 @@ import os
 import pyrevit.coreutils.git as libgit
 from pyrevit.versionmgr import updater
 from pyrevit import EXEC_PARAMS
-from pyrevit import script
 from pyrevit import forms
-from pyrevit.loader import sessionmgr
-from pyrevit.loader import sessioninfo
+
 
 from dosymep_libs.bim4everyone import *
 
 __cleanengine__ = True
+
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
@@ -25,15 +24,6 @@ def script_execute(plugin_logger):
     repo_path = libgit.libgit.Repository.Discover(path)
     repo_info = libgit.get_repo(repo_path)
     updater.update_repo(repo_info)
-
-    logger = script.get_logger()
-    results = script.get_results()
-
-    # re-load pyrevit session.
-    logger.info('Reloading....')
-    sessionmgr.load_session()
-
-    results.newsession = sessioninfo.get_session_uuid()
 
 
 script_execute()
